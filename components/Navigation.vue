63b4aa42b91a9103da8d7beac693cf3394afa03e
xdfkenny <xdfkenny@gmail.com>
2026-02-10 09:24:50 -0400

    asdasdasdasdasd

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { Home, Users, Calendar, BookOpen, Image, Instagram, Menu, X } from 'lucide-vue-next'
+import { Home, Users, Calendar, BookOpen, Image, Instagram } from 'lucide-vue-next'
 const route = useRoute()
 const isActive = (path: string) => route.path === path
 
@@ -105,11 +105,11 @@ watch(() => route.path, () => {
       <!-- Mobile Hamburger Button -->
       <button 
         @click="toggleMenu" 
-        class="lg:hidden z-50 p-2 text-black hover:bg-gray-100 rounded-lg transition-colors"
+        class="lg:hidden z-50 p-2 text-black hover:bg-gray-100 rounded-lg transition-colors flex items-center justify-center"
         aria-label="Toggle Menu"
       >
-        <Menu v-if="!isMenuOpen" class="w-8 h-8" />
-        <X v-else class="w-8 h-8" />
+        <svg v-if="!isMenuOpen" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><g fill="none"><path d="m12.593 23.258l-.011.002l-.071.035l-.02.004l-.014-.004l-.071-.035q-.016-.005-.024.005l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427q-.004-.016-.017-.018m.265-.113l-.013.002l-.185.093l-.01.01l-.003.011l.018.43l.005.012l.008.007l.201.093q.019.005.029-.008l.004-.014l-.034-.614q-.005-.018-.02-.022m-.715.002a.02.02 0 0 0-.027.006l-.006.014l-.034.614q.001.018.017.024l.015-.002l.201-.093l.01-.008l.004-.011l.017-.43l-.003-.012l-.01-.01z"/><path fill="currentColor" d="M20 17.5a1.5 1.5 0 0 1 .144 2.993L20 20.5H4a1.5 1.5 0 0 1-.144-2.993L4 17.5zm0-7a1.5 1.5 0 0 1 0 3H4a1.5 1.5 0 0 1 0-3zm0-7a1.5 1.5 0 0 1 0 3H4a1.5 1.5 0 1 1 0-3z"/></g></svg>
+        <svg v-else xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="currentColor" d="m12 13.4l-4.9 4.9q-.275.275-.7.275t-.7-.275t-.275-.7t.275-.7l4.9-4.9l-4.9-4.9q-.275-.275-.275-.7t.275-.7t.7-.275t.7.275l4.9 4.9l4.9-4.9q.275-.275.7-.275t.7.275t.275.7t-.275.7L13.4 12l4.9 4.9q.275.275.275.7t-.275.7t-.7.275t-.7-.275z"/></svg>
       </button>
 
       <!-- Mobile Menu Overlay -->
